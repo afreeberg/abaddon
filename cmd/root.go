cc0a28cce226a70f35addee90f3508dae8952410
afreeberg <andrew.a.freeberg@gmail.com>
2022-04-04 12:30:19 -0400

    Updated root.go with better command description.

--- cmd/root.go
+++ cmd/root.go
@@ -34,13 +34,14 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "abaddon",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "A WIP package for managing... stuff? as I learn go.",
+	Long: `Using this as a centralized location to implement helpful utilities as I expand my knowledge of:
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	* Go
+	* Kubernetes
+	* Home Automation
+	* etc
+	`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
